19354111b594e895451ad22e5174ba4377c3ac82
IcyBlonde <haynesad4@gmail.com>
2025-01-30 11:23:34 -0500

    setByte works but has an if statement

--- Tools.C
+++ Tools.C
@@ -245,7 +245,13 @@ uint64_t Tools::copyBits(uint64_t source, uint64_t dest,
  */
 uint64_t Tools::setByte(uint64_t source, int32_t byteNum)
 {
-  return 0;
+  long mask = 0;
+    
+  if (byteNum >= 0 && byteNum <= 7) {
+    mask = ((1ULL << 8) - 1) << (byteNum * 8);
+  }
+  
+  return source | mask;
 }
 
 
